3d46901588e88c698c82b777634006f19f422e63
sam-vergo <samvong03@gmail.com>
2023-12-13 02:35:26 +0800

    Commit 2: Added Fragments consisting of Timer (Home Page), Tasks and Settings

--- app/src/main/java/com/example/taskflow/MainActivity.kt
+++ app/src/main/java/com/example/taskflow/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.taskflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.taskflow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.homeButton.setOnClickListener {
+            replaceFragment(Home())
+        }
+
+        binding.tasksButton.setOnClickListener {
+            replaceFragment(Tasks())
+        }
+
+        binding.settingsButton.setOnClickListener {
+            replaceFragment(Settings())
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+
     }
 }
